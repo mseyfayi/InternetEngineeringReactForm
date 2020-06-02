0147cc18e1c4f2ca24daa01b46e0debb0a131aa3
madsams024 <madsams024@gmail.com>
2020-06-03 00:59:18 +0430

    wip

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "../../global/components/Header";
-import renderToast from "../../global/actions/renderToast";
+import {renderToast} from "../../global/actions";
 
 const Home = () => {
     const handleClick = () => renderToast('koft', 'warning', 'warning');
     return (
-        <div>
+        <>
             <Header text='Home' isHome/>
-            <div className='d-flex align-items-center justify-content-center' onClick={handleClick}>
+            <div className='d-flex flex-grow-1 align-items-center justify-content-center' onClick={handleClick}>
                 Show toast
             </div>
-        </div>
+        </>
     )
 };
 export default Home
