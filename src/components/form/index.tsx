f615bb1cba8256632b045df3b7b75f81e41be10b
madsams024 <madsams024@gmail.com>
2020-06-03 06:40:05 +0430

    form id added to header

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../../global/components/Header";
 import FormContainer from "./FormContainer";
+import {useParams} from "react-router";
 
-const Form = () =>
-    <>
-        <Header text='Form'/>
+const Form = () => {
+    const {id} = useParams();
+
+    return <>
+        <Header text={`Form ${id}`}/>
         <FormContainer/>
     </>;
+};
 export default Form
