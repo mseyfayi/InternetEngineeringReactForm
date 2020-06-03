8859f67d7ce888be1d8ff33f7a30bec0ba86d2fb
madsams024 <madsams024@gmail.com>
2020-06-04 01:55:31 +0430

    FormFieldInputLocation.tsx type of value changed

--- src/components/form/formField/FormFieldInputLocation.tsx
+++ src/components/form/formField/FormFieldInputLocation.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {TextField} from "@material-ui/core";
 import FormLocationDialog from './FormLocationDialog'
-import {IFormInputPropsType} from "../../../global/types";
+import {IFormInputPropsType, latLng} from "../../../global/types";
 
+interface PropsType extends IFormInputPropsType {
+    value: latLng | null;
+}
 
 const FormFieldInputLocation = ({name, title, value, onChange}: IFormInputPropsType) => {
     const [open, setOpen] = React.useState(false);
@@ -17,7 +20,7 @@ const FormFieldInputLocation = ({name, title, value, onChange}: IFormInputPropsT
                 InputLabelProps={{shrink: (!!value)}}
                 label={title}
                 id={name}
-                value={value}
+                value={value && `(${value.lat} ${value.lng})`}
                 onClick={handleOpen}
             />
             <FormLocationDialog save={onChange} onClose={handleClose} open={open}/>
